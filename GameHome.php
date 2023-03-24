cd75fa81f7ac388504d3101f854624eb44ffb124
melodieot <melodie.olory@gmail.com>
2023-03-24 13:51:54 +0100

    ajout page jeu et home, merge

---
+++ GameHome.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>BombParty</title>
+</head>
+<body>
+    <h1 class="title">Bienvenue sur BombParty !</h1>
+
+    <div class="startDiv">
+        <button class="btnStart">
+            <span>Start</span>
+                <svg viewBox="0 0 13 10" height="10px" width="15px">
+                    <path d="M1,5 L11,5"></path>
+                    <polyline points="8 1 12 5 8 9"></polyline>
+                </svg>
+        </button>
+    </div>
+
+</body>
+</html>
