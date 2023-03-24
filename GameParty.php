b32fac35cf86408d4ab6c4f6f50348c06bc21f1c
melodieot <melodie.olory@gmail.com>
2023-03-24 11:31:16 +0100

    ajout bg, sprite bombe

--- GameParty.php
+++ GameParty.php
@@ -4,12 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.scss">
     <title>BombParty</title>
 </head>
 <body>
-    <h1>bienvenue</h1>
-  <img src="img/Bombe.png" alt="" srcset="">
-    
+    <h1>Bienvenue sur BombParty</h1>
+    <img src="img/bombe.png" alt="" srcset="">
+    <button class="btnStart">
+        <span>Start</span>
+            <svg viewBox="0 0 13 10" height="10px" width="15px">
+                <path d="M1,5 L11,5"></path>
+                <polyline points="8 1 12 5 8 9"></polyline>
+            </svg>
+    </button>
 </body>
 </html>
